30ad5b99ebee8410aa5271099a588a9d90725506
Yusuf <yusufh97@live.com>
2018-02-24 22:40:03 -0800

    pdy12

---
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+using System.Linq;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Controllers
+{
+   public class HomeController : Controller
+   {
+      private SchoolContext db = new SchoolContext();
+
+      public ActionResult Index()
+      {
+         return View();
+      }
+
+      public ActionResult About()
+      {
+         // IQueryable<EnrollmentDateGroup> data = from student in db.Students
+         //      group student by student.EnrollmentDate into dateGroup
+         //    select new EnrollmentDateGroup()
+         //  {
+         //    EnrollmentDate = dateGroup.Key,
+         //  StudentCount = dateGroup.Count()
+         //   };
+         string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+        + "FROM Person "
+        + "WHERE Discriminator = 'Student' "
+                + "GROUP BY EnrollmentDate";
+         IEnumerable<EnrollmentDateGroup> data = db.Database.SqlQuery<EnrollmentDateGroup>(query);
+
+         return View(data.ToList());
+      }
+
+      public ActionResult Contact()
+      {
+         ViewBag.Message = "Your contact page.";
+
+         return View();
+      }
+
+      protected override void Dispose(bool disposing)
+      {
+         db.Dispose();
+         base.Dispose(disposing);
+      }
+   }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+   public class Student : Person
+   {
+      //public string EmailAddress { get; set; }
+      [DataType(DataType.Date)]
+      [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+      [Display(Name = "Enrollment Date")]
+      public DateTime EnrollmentDate { get; set; }
+
+      public virtual ICollection<Enrollment> Enrollments { get; set; }
+   }
+}
